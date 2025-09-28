2f2d0d51411e56c75eec150c50605a96a3071ce8
jajohnson05 <jajohnson05@wm.edu>
2025-09-28 04:23:54 -0400

    Adding winning and losing features

--- gamestate.py
+++ gamestate.py
@@ -3,6 +3,7 @@ class GameState:
         self.stash = 0
         self.level = 1
         self.game_over = False
+        self.win=False
         self.paused = False
         self.time_left=10
         self.high_score = 0
